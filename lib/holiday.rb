98aaa57fee659711b13c482ef13d22b5da646c0e
Linda Chang <lindachang186@gmail.com>
2018-10-19 14:26:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,10 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key}:".capitalize!
     value.each do |holiday, supply|
-      array = "#{holiday}".split("_")
+      array = "#{holiday}".split("_ ")
         array.collect do |item|
           item.capitalize!
-          array.join("_")
+          array.join(" ")
           puts "  #{item}: " + supply.join(", ")
         end
     end
