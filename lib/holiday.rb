e746ad08a33c52d996a70ca9e0da5524e1db29fa
Linda Chang <lindachang186@gmail.com>
2018-10-19 13:42:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    puts "#{key}:".capitalize
+    puts "#{key}:".capitalize!
     value.each do |holiday, supply|
       array = []
       array << "#{holiday}".split(" ")
