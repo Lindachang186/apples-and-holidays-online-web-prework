e52f144cd5921ae5f0ea03b7187a96162e1651e9
Linda Chang <lindachang186@gmail.com>
2018-10-19 15:10:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |key, value|
     value.each do |holiday, supply|
-      holiday.each do |item|
+      supply.each do |item|
       new = []
       if item == "BBQ"
         new.push("#{holiday}")
