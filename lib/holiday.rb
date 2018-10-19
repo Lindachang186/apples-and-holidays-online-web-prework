ffab414cf69b080334644b5d5d0dceed727e2d20
Linda Chang <lindachang186@gmail.com>
2018-10-19 13:48:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
       array = "#{holiday}".split(" ")
         array.collect do |item|
           item.capitalize!
-          item.join(" ")
-          item
+          array.join(" ")
+          array 
         end
     end
   end
