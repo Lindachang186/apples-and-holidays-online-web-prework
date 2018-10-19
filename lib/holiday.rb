68c9cf88f54f6aaf5bfd2409f4e89622c38f9a7b
Linda Chang <lindachang186@gmail.com>
2018-10-19 14:18:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
           binding.pry
           item.capitalize!
           array.join(" ")
-          puts "  #{item}: " + supply
+          puts "  #{item}: " + "#{supply}"
         end
     end
   end
