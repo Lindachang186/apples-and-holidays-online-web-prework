dfbb72061e3944fc1610f8bd40abadfc400ab4c4
Linda Chang <lindachang186@gmail.com>
2018-10-19 13:51:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
           item.capitalize!
           array.join(" ")
           array
-            supply.values
+            supply.to_s.values
         end
     end
   end
